894e69d341957442c352a7e2de19b18b7ed32d1e
Van-Romel A S Neto <vanr.neto@hotmail.com>
2022-02-22 11:03:24 -0300

    finished project

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ let food = {
 }
 
 function createBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "green";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function createSnake() {
     for (let i = 0; i < snake.length; i++) {
-        context.fillStyle = "green";
+        context.fillStyle = "lightgreen";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
